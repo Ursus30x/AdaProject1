9e16ea05cf09c03dfa18d15fc160e21f15a3f0cb
Bigosssss <jakub.drejka@gmail.com>
2024-10-14 20:41:19 +0200

    test

--- src/simulation.adb
+++ src/simulation.adb
@@ -10,8 +10,8 @@ procedure Simulation is
 
    ----GLOBAL VARIABLES---
 
-   Number_Of_Producers: constant Integer := 5;
-   Number_Of_Assemblies: constant Integer := 3;
+   Number_Of_Producers: constant Integer := 8;
+   Number_Of_Assemblies: constant Integer := 4;
    Number_Of_Consumers: constant Integer := 2;
    Number_Of_Days: constant Integer := 7;
 
@@ -22,9 +22,8 @@ procedure Simulation is
 
    --each Producer is assigned a Product that it produces
 
-   Product_Name: constant array (Producer_Type) of String(1 .. 10)
-     := ("Jablko    ", "Pomarancza", "Mango     ", "Arbuz     ",
-         "Sliwka    ", "Ananas    ", "Cytryna   ", "Kaktus    ");
+   Product_Name: constant array (Producer_Type) of String(1 .. 3)
+     := ("Jab", "Pom", "Man", "Arb", "Sli", "Ana", "Cyt", "Kak");
    --Assembly is a collection of products
    Assembly_Name: constant array (Assembly_Type) of String(1 .. 16)
      := ("Sok_JabPomManArb", "Sok_SliAnaCytKak", "Sok_ManArbCytKak", "Sok_JabPomSliAna");
@@ -52,7 +51,7 @@ procedure Simulation is
       -- Deliver an assembly (provided there are enough products for it)
       entry Deliver(Assembly: in Assembly_Type; Number: out Integer);
 
-      entry Sunday();
+      --entry Sunday(Day: in Days_Type);
    end Buffer;
 
    task type Calendar is
@@ -151,14 +150,15 @@ procedure Simulation is
       Storage_Capacity: constant Integer := 30;
       type Storage_type is array (Producer_Type) of Integer;
       Storage: Storage_type
-        := (0, 0, 0, 0, 0);
+        := (0, 0, 0, 0, 0, 0, 0, 0);
       Assembly_Content: array(Assembly_Type, Producer_Type) of Integer
-        := ((2, 1, 2, 0, 2),
-            (1, 2, 0, 1, 0),
-            (3, 2, 2, 0, 1));
+        := ((2, 1, 2, 0, 2, 2, 1, 2),
+            (1, 2, 0, 1, 0, 1, 2, 1),
+            (3, 2, 2, 0, 1, 0, 1, 0),
+            (1, 2, 3, 1, 0, 2, 1, 0));
       Max_Assembly_Content: array(Producer_Type) of Integer;
       Assembly_Number: array(Assembly_Type) of Integer
-        := (1, 1, 1);
+        := (1, 1, 1, 1);
       In_Storage: Integer := 0;
 
       procedure Setup_Variables is
